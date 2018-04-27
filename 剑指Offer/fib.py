8e548db8efc11d688853002f137676e29caef1ab
itswcg <itswcg@gmail.com>
2018-04-27 23:08:21 +0800

    斐波那契数列

---
+++ 剑指Offer/fib.py
@@ -0,0 +1,18 @@
+class Solution:
+    def Fibonacci(self, n):
+        l = [0, 1]
+        for i in range(2, n+1):
+            l.append(l[i-1]+l[i-2])
+        return l[n]
+
+    def fib1(self, n):
+        a, b = 0, 1
+        for i in range(n-2):
+            a, b = b, a+b
+        return b
+
+    def fib2(self, n):
+        if n < 2:
+            return n
+        else:
+            return self.fib2(n-1) + self.fib2(n-2)
