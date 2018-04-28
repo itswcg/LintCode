cf2db38710f8abd0275e4369f759483d6620a0bf
itswcg <itswcg@gmail.com>
2018-04-28 12:16:28 +0800

    重建二叉树

---
+++ 剑指Offer/reConstructBinaryTree.py
@@ -0,0 +1,15 @@
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    # 返回构造的TreeNode根节点
+    def reConstructBinaryTree(self, pre, tin):
+        if not pre or not tin:
+            return
+        root = TreeNode(pre[0])
+        i = tin.index(pre[0])
+        root.left = self.reConstructBinaryTree(pre[1:i+1], tin[:i])
+        root.right = self.reConstructBinaryTree(pre[i+1:], tin[i+1:])
+        return root
