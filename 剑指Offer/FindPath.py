1ecf48fc21871dca97e0c52578400fe947a80670
itswcg <itswcg@gmail.com>
2018-04-29 20:59:09 +0800

    二叉树和路径

---
+++ 剑指Offer/FindPath.py
@@ -0,0 +1,17 @@
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    # 返回二维列表，内部每个列表表示找到的路径
+    def FindPath(self, root, expectNumber):
+        if root is None: return []
+        if root and root.left is None and root.right is None and root.val == expectNumber:
+            return [[root.val]]
+        res = []
+        left = self.FindPath(root.left, expectNumber-root.val)
+        right = self.FindPath(root.right, expectNumber-root.val)
+        for i in left+right:
+            res.append([root.val]+i)
+        return res
